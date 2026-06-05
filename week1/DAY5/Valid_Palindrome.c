b13a5a59cccc1523f1c60a8eb6a94ce2ae439121
harshit-033 <harshit.k.singh03102004@gmail.com>
2026-06-05 14:42:41 +0530

    update

--- week1/DAY5/Valid_Palindrome.c
+++ week1/DAY5/Valid_Palindrome.c
@@ -1,3 +1,33 @@
-//
-// Created by harsh on 05-06-2026.
-//
+bool isPalindrome(char* s) {
+
+
+    int j=strlen(s)-1;
+    int count=0;
+    int i=0;
+    while(i<j){
+        if ((s[i]<65 || s[i]>90) && (s[i]<97 || s[i]>122) &&(s[i]<48 || s[i]>57)){
+            i++;
+            continue;
+        }
+        if ((s[j]<65 || s[j]>90) && (s[j]<97 || s[j]>122)&&(s[j]<48 || s[j]>57)){
+            j--;
+            continue;
+        }
+        s[i]=tolower(s[i]);
+        s[j]=tolower(s[j]);
+        if(s[i]!=s[j]){
+            count++;
+            break;
+        }
+        i++;
+        j--;
+    }
+
+    if(count==0){
+        return true;
+    }
+    else{
+        return false;
+
+    }
+}
--- week1/DAY5/longest_common_prefix.c
+++ week1/DAY5/longest_common_prefix.c
@@ -1,3 +1,19 @@
-//
-// Created by harsh on 05-06-2026.
-//
+
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+
+    int len=strlen(strs[0]);
+    char *ans=strs[0];
+
+    for(int i=1;i<strsSize;i++) {
+        int j=0;
+
+        while(ans[j] && strs[i][j] && ans[j]==strs[i][j]) {
+            j++;
+        }
+
+        ans[j]='\0';
+    }
+
+    return ans;
+}
